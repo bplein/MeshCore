b6323a6a3eec5009a6d999f0f3d9f82a55b2c962
Bill Plein <260078+bplein@users.noreply.github.com>
2026-06-14 19:47:09 -0500

    Implement nRF52 Power Management for Ikoka Nano
    Implement nRF52 Power Management for Ikoka Nano, matching the code in the xiao_nrf variant that shares the same Seeed Xiao NRF component.
    
    Also cleaned up some odd tabbing (3 spaces?) from someone else's PR.

--- variants/ikoka_nano_nrf/IkokaNanoNRFBoard.h
+++ variants/ikoka_nano_nrf/IkokaNanoNRFBoard.h
@@ -4,9 +4,18 @@
 #include <Arduino.h>
 #include <helpers/NRF52Board.h>
 
+#ifndef USER_BTN_PRESSED
+#define USER_BTN_PRESSED LOW
+#endif
+
 #ifdef XIAO_NRF52
 
 class IkokaNanoNRFBoard : public NRF52BoardDCDC {
+protected:
+#if NRF52_POWER_MANAGEMENT
+  void initiateShutdown(uint8_t reason) override;
+#endif
+
 public:
   IkokaNanoNRFBoard() : NRF52Board("XIAO_NRF52_OTA") {}
   void begin();
@@ -15,34 +24,20 @@ public:
   void onBeforeTransmit() override {
     digitalWrite(P_LORA_TX_LED, LOW);   // turn TX LED on
     #if defined(LED_BLUE)
-       // turn off that annoying blue LED before transmitting
-       digitalWrite(LED_BLUE, HIGH);
+      // turn off that annoying blue LED before transmitting
+      digitalWrite(LED_BLUE, HIGH);
     #endif
   }
   void onAfterTransmit() override {
     digitalWrite(P_LORA_TX_LED, HIGH);   // turn TX LED off
     #if defined(LED_BLUE)
        // do it after transmitting too, just in case
-       digitalWrite(LED_BLUE, HIGH);
+      digitalWrite(LED_BLUE, HIGH);
     #endif
   }
 #endif
 
-  uint16_t getBattMilliVolts() override {
-    // Please read before going further ;)
-    // https://wiki.seeedstudio.com/XIAO_BLE#q3-what-are-the-considerations-when-using-xiao-nrf52840-sense-for-battery-charging
-
-    // We can't drive VBAT_ENABLE to HIGH as long 
-    // as we don't know whether we are charging or not ...
-    // this is a 3mA loss (4/1500)
-    digitalWrite(VBAT_ENABLE, LOW);
-    int adcvalue = 0;
-    analogReadResolution(12);
-    analogReference(AR_INTERNAL_3_0);  
-    delay(10);
-    adcvalue = analogRead(PIN_VBAT);
-    return (adcvalue * ADC_MULTIPLIER * AREF_VOLTAGE) / 4.096;
-  }
+  uint16_t getBattMilliVolts() override;
 
   const char *getManufacturerName() const override {
     return MANUFACTURER_STRING;
