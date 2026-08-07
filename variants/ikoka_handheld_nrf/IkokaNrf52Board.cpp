558c882dd0714115e2a73aaa08554f8e8b5d50f6
Bill Plein <260078+bplein@users.noreply.github.com>
2026-08-07 23:22:22 +0000

    port xiao NRF52_POWER_MANAGEMENT + button handling
    Port xiao's power-management and button handling into the ikoka handheld
    variant while preserving ikoka-specific behavior:
    
    - Leave battery functions untouched, as the Ikoka Handheld has its own
      battery charging and management circuit.
    - Power management copied from xiao (BAT+ on battery; boot-voltage
      protection is safe with the tp4056 solar charging).

--- variants/ikoka_handheld_nrf/IkokaNrf52Board.cpp
+++ variants/ikoka_handheld_nrf/IkokaNrf52Board.cpp
@@ -5,6 +5,30 @@
 
 #include "IkokaNrf52Board.h"
 
+#ifdef NRF52_POWER_MANAGEMENT
+// Static configuration for power management
+// Values set in variant.h defines
+const PowerMgtConfig power_config = {
+  .lpcomp_ain_channel = PWRMGT_LPCOMP_AIN,
+  .lpcomp_refsel = PWRMGT_LPCOMP_REFSEL,
+  .voltage_bootlock = PWRMGT_VOLTAGE_BOOTLOCK
+};
+
+void IkokaNrf52Board::initiateShutdown(uint8_t reason) {
+  bool enable_lpcomp = (reason == SHUTDOWN_REASON_LOW_VOLTAGE ||
+                        reason == SHUTDOWN_REASON_BOOT_PROTECT);
+
+  pinMode(VBAT_ENABLE, OUTPUT);
+  digitalWrite(VBAT_ENABLE, enable_lpcomp ? LOW : HIGH);
+
+  if (enable_lpcomp) {
+    configureVoltageWake(power_config.lpcomp_ain_channel, power_config.lpcomp_refsel);
+  }
+
+  enterSystemOff(reason);
+}
+#endif // NRF52_POWER_MANAGEMENT
+
 void IkokaNrf52Board::begin() {
   NRF52BoardDCDC::begin();
 
@@ -23,6 +47,10 @@ void IkokaNrf52Board::begin() {
   // required button pullup is handled as part of button initilization
   // in target.cpp
 
+#ifdef PIN_USER_BTN
+  pinMode(PIN_USER_BTN, INPUT_PULLUP);
+#endif
+
 #if defined(PIN_WIRE_SDA) && defined(PIN_WIRE_SCL)
   Wire.setPins(PIN_WIRE_SDA, PIN_WIRE_SCL);
 #endif
@@ -34,7 +62,25 @@ void IkokaNrf52Board::begin() {
   digitalWrite(P_LORA_TX_LED, !LED_STATE_ON);   // boot idle = OFF (HIGH for active-LOW)
 #endif
 
-  delay(10); // give sx1262 some time to power up
+#ifdef NRF52_POWER_MANAGEMENT
+  // Boot voltage protection check (may not return if voltage too low)
+  checkBootVoltage(&power_config);
+#endif
+
+  delay(10);  // Give sx1262 some time to power up
+}
+
+uint16_t IkokaNrf52Board::getBattMilliVolts() {
+  // https://wiki.seeedstudio.com/XIAO_BLE#q3-what-are-the-considerations-when-using-xiao-nrf52840-sense-for-battery-charging
+  // VBAT_ENABLE must be LOW to read battery voltage
+  // ikoka's begin() does NOT configure the ADC (unlike xiao), so set it here
+  digitalWrite(VBAT_ENABLE, LOW);
+  int adcvalue = 0;
+  analogReadResolution(12);
+  analogReference(AR_INTERNAL_3_0);
+  delay(10);
+  adcvalue = analogRead(PIN_VBAT);
+  return (adcvalue * ADC_MULTIPLIER * AREF_VOLTAGE) / 4.096;
 }
 
 #endif
--- variants/ikoka_handheld_nrf/IkokaNrf52Board.h
+++ variants/ikoka_handheld_nrf/IkokaNrf52Board.h
@@ -4,9 +4,18 @@
 #include <MeshCore.h>
 #include <helpers/NRF52Board.h>
 
+#ifndef USER_BTN_PRESSED
+#define USER_BTN_PRESSED LOW
+#endif
+
 #ifdef IKOKA_NRF52
 
 class IkokaNrf52Board : public NRF52BoardDCDC {
+protected:
+#if NRF52_POWER_MANAGEMENT
+  void initiateShutdown(uint8_t reason) override;
+#endif
+
 public:
   IkokaNrf52Board() : NRF52Board("XIAO_NRF52_OTA") {}
   void begin();
@@ -20,24 +29,28 @@ public:
   }
 #endif
 
-  uint16_t getBattMilliVolts() override {
-    // Please read before going further ;)
-    // https://wiki.seeedstudio.com/XIAO_BLE#q3-what-are-the-considerations-when-using-xiao-nrf52840-sense-for-battery-charging
-
-    // We can't drive VBAT_ENABLE to HIGH as long
-    // as we don't know whether we are charging or not ...
-    // this is a 3mA loss (4/1500)
-    digitalWrite(VBAT_ENABLE, LOW);
-    int adcvalue = 0;
-    analogReadResolution(12);
-    analogReference(AR_INTERNAL_3_0);
-    delay(10);
-    adcvalue = analogRead(PIN_VBAT);
-    return (adcvalue * ADC_MULTIPLIER * AREF_VOLTAGE) / 4.096;
-  }
+  uint16_t getBattMilliVolts() override;
 
   const char* getManufacturerName() const override {
     return "Ikoka Handheld E22 30dBm (Xiao_nrf52)";
+  }
+
+  void powerOff() override {
+    // set led on and wait for button release before poweroff
+    digitalWrite(PIN_LED, LOW);
+#ifdef PIN_USER_BTN
+    while(digitalRead(PIN_USER_BTN) == USER_BTN_PRESSED);
+#endif
+    digitalWrite(LED_GREEN, HIGH);
+    digitalWrite(LED_BLUE, HIGH);
+    digitalWrite(PIN_LED, HIGH);
+
+#ifdef PIN_USER_BTN
+    // configure button press to wake up when in powered off state
+    nrf_gpio_cfg_sense_input(digitalPinToInterrupt(g_ADigitalPinMap[PIN_USER_BTN]), NRF_GPIO_PIN_PULLUP, NRF_GPIO_PIN_SENSE_LOW);
+#endif
+
+    NRF52Board::powerOff();
   }
 };
 
